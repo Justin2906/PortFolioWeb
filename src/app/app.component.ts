894cac743204cbdd755fa85965a39d9dacda2a23
Justin Rondon <rondonjustin643@hotmail.com>
2024-04-30 14:30:37 +0200

    subiendo cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from "@angular/common";
+import { MainComponent } from './components/main/main.component';
+import { ExpertiseComponent } from './components/expertise/expertise.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    NavbarComponent,
+    MainComponent,
+    ExpertiseComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
